c34fe426702babe76cdf7f0c9d73a3576529c58c
fpeng-02 <fpeng2002@gmail.com>
2020-12-19 20:33:59 -0800

    fixes

--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-abstract class Entity : MonoBehaviour
+public abstract class Entity : MonoBehaviour
 {
     private double hitPoints;
     private readonly string name; 
