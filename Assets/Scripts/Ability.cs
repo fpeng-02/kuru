49d5e98f3c54ea6fc8f540c7438d5ee226008347
fpeng-02 <fpeng2002@gmail.com>
2020-12-19 20:42:52 -0800

    bossscript

--- Assets/Scripts/Ability.cs
+++ Assets/Scripts/Ability.cs
@@ -13,7 +13,6 @@ public abstract class Ability : MonoBehaviour
     public double getCooldownProgress() { return cooldownProgress; }
     public void setCooldownProgress(double cool) { cooldownProgress = cool; }
     public string getName() { return name; }
-
     // Start is called before the first frame update
     void Start()
     {
