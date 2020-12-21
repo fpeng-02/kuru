b9c3caa482676cbbe5e72b7350dc7e2be74035bc
fpeng-02 <fpeng2002@gmail.com>
2020-12-21 00:51:17 -0800

    Minor Adjustments to Boss and Bullets

--- Assets/Scripts/Abilities/TutorialSingleShot.cs
+++ Assets/Scripts/Abilities/TutorialSingleShot.cs
@@ -16,7 +16,7 @@ public class TutorialSingleShot : GenericAbility
     {
         Vector3 playerPos = GameObject.Find("Player").transform.position;
         playerPos.z = 0.0F;
-        Vector3 dirVector = Camera.main.ScreenToWorldPoint(playerPos) - this.transform.position;
+        Vector3 dirVector = playerPos - this.transform.position;
         dirVector.z = 0;
         dirVector = dirVector.normalized;
         float angle = Mathf.Atan2(dirVector.y, dirVector.x) * Mathf.Rad2Deg;
@@ -24,6 +24,6 @@ public class TutorialSingleShot : GenericAbility
     }
     public override float getInterval(int index)
     {
-        return 0.5f;
+        return 0.1f;
     }
 }
