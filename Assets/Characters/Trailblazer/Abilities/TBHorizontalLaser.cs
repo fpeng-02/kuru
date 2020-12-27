b24c43394e86b3d640167ed4689e0c50e62a9fe6
Daniel Xie <danielyxie2002@gmail.com>
2020-12-26 17:20:19 -0800

    Trailblazer - Phase 3 horiz+vert lasers
    - also fixed loops in phases 1 and 2

---
+++ Assets/Characters/Trailblazer/Abilities/TBHorizontalLaser.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TBHorizontalLaser : AbilitySequence
+{
+    [SerializeField] private List<Effect> effectList = new List<Effect>();
+    [SerializeField] private float chargeTime;
+    [SerializeField] private float damageInterval;
+    [SerializeField] private float beamPrepWidth;
+    [SerializeField] private float beamFireWidth;
+    private LineRenderer lr;
+
+
+    // set "lr" in start
+    void Start()
+    {
+        lr = this.gameObject.GetComponent<LineRenderer>();
+        lr.enabled = false;
+    }
+
+    public Vector3 getSpawnVector()
+    {
+        return this.transform.position;
+    }
+
+    enum HorizontalDirection { Right, Left }
+
+    public override IEnumerator cast()
+    {
+        HorizontalDirection castDir = (HorizontalDirection)Random.Range(0, 2);
+        Vector3 playerPos = GameObject.Find("Player").transform.position;
+        Vector3 randomPos = new Vector3(0, 0, 0);
+
+        // teleport to a random spot along the perimeter of the 9 islands
+        // TODO: replace these magic numbers lol
+        randomPos.x = castDir == HorizontalDirection.Right ? -5 : 5;
+        randomPos.y = Mathf.Round(playerPos.y / 3.0f) * 3;
+        this.transform.position = randomPos;
+
+        // prep laser 
+        lr.enabled = true;
+        Vector2 castDirVector = castDir == HorizontalDirection.Right ? Vector2.right : Vector2.left;
+        RaycastHit2D initHit = Physics2D.Raycast(this.transform.position, castDirVector, 50.0f, LayerMask.GetMask("Environment"), -100, 100);
+        lr.startWidth = beamPrepWidth;
+        lr.endWidth = beamPrepWidth;
+        lr.SetPosition(0, this.transform.position);
+        lr.SetPosition(1, initHit.point);
+        yield return new WaitForSeconds(chargeTime);
+
+        // fire laser
+        lr.startWidth = beamFireWidth;
+        lr.endWidth = beamFireWidth;
+        Vector3 laserDistance = new Vector3(transform.position.x - initHit.point.x, transform.position.y - initHit.point.y, 0);
+        float fireUptime = this.getUptime() - chargeTime;
+        List<RaycastHit2D> rayResults = new List<RaycastHit2D>();
+        ContactFilter2D cf = new ContactFilter2D();
+        cf.useLayerMask = true;
+        //Filter raycast to only hit ents (rip trees)
+        cf.layerMask = LayerMask.GetMask("Entity", "Player");
+        while (fireUptime >= 0) {
+            yield return new WaitForSeconds(damageInterval);
+            Physics2D.CircleCast(this.transform.position, beamFireWidth / 2, playerPos, cf, rayResults, Vector3.Magnitude(laserDistance));
+            foreach (RaycastHit2D curHit in rayResults) {
+                Entity curEnt = curHit.transform.gameObject.GetComponent<Entity>();
+                // damage :)
+            }
+            fireUptime -= damageInterval;
+            rayResults.Clear();
+        }
+
+        lr.enabled = false;
+        //GameObject.Destroy(this.gameObject.GetComponent<LineRenderer>());
+        //Debug.Log(this.GetComponent<LineRenderer>());
+        yield return null;
+    }
+
+    public void turnOff()
+    {
+        lr.enabled = false;
+    }
+
+    void Update()
+    {
+        if (lr.enabled) {
+            lr.SetPosition(0, this.transform.position);
+        }
+    }
+}
--- Assets/Characters/Trailblazer/Phases/TBPhase1.cs
+++ Assets/Characters/Trailblazer/Phases/TBPhase1.cs
@@ -20,17 +20,19 @@ public class TBPhase1 : Phase
 
     public override IEnumerator phaseLoop()
     {
-        yield return new WaitForSeconds(interval);
+        while (true) {
+            yield return new WaitForSeconds(interval);
 
-        // choose a random position to teleport to, but make sure it's far enough away from the player
-        Vector3 playerPos = GameObject.Find("Player").transform.position;
-        Vector3 randomPos = playerPos;
-        while ((randomPos - playerPos).magnitude < playerSafeRadius) {
-            randomPos = new Vector3(Random.Range(xLowerBound, xUpperBound), Random.Range(yLowerBound, yUpperBound), 0);
+            // choose a random position to teleport to, but make sure it's far enough away from the player
+            Vector3 playerPos = GameObject.Find("Player").transform.position;
+            Vector3 randomPos = playerPos;
+            while ((randomPos - playerPos).magnitude < playerSafeRadius) {
+                randomPos = new Vector3(Random.Range(xLowerBound, xUpperBound), Random.Range(yLowerBound, yUpperBound), 0);
+            }
+            this.transform.position = randomPos;
+            yield return new WaitForSeconds(waitAfterTeleport);
+            // then shoot a shotgun
+            owner.cast("Shotgun");
         }
-        this.transform.position = randomPos;
-        yield return new WaitForSeconds(waitAfterTeleport);
-        // then shoot a shotgun
-        owner.cast("Shotgun");
     }
 }
