65b5d569c31fa8932884318f0b865b5e23473579
Daniel Xie <danielyxie2002@gmail.com>
2020-12-29 19:57:34 -0800

    Polygon - dice roll
    still needs the action after dice roll

--- Assets/Characters/Polygon/Phases/SquarePhase.cs
+++ Assets/Characters/Polygon/Phases/SquarePhase.cs
@@ -5,21 +5,56 @@ using UnityEngine;
 public class SquarePhase : Phase
 {
     [SerializeField] private Sprite[] diceSprites = new Sprite[6];
+    [SerializeField] private int bounces;
+    [SerializeField] private float speed;
+    private Rigidbody2D rb2d;
+    private SpriteRenderer spriteRenderer;
+    private int curBounces;
+    private bool rolling;
+    private int rollNumber;
 
     public override IEnumerator beginPhase()
     {
+        rb2d = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
         yield return phaseLoop();
     }
 
     public override IEnumerator phaseLoop()
     {
-        // MOVEMENT
-        // move in a straight line
-        // when hit wall, reflect off and bonk on walls
+        while (true) {
+            // MOVEMENT
+            // move in a straight line
+            // when hit wall, reflect off and bonk on walls (physics mat should handle this)
+            curBounces = 0;
+            rb2d.drag = 0;
+            rb2d.angularDrag = 0;
+            rolling = true;
+            rb2d.velocity = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized * speed;
+            yield return new WaitUntil(() => rolling == false);
 
-        // ATTACK
-        // choose what to spawn based on roll number
-        // stay still? idk lol
-        yield break;
+            // ATTACK
+            // choose what to spawn based on roll number
+            // stay still? idk lol
+            yield return new WaitForSeconds(2.0f);
+        }
+    }
+
+    void OnCollisionEnter2D()
+    {
+        rollNumber = Random.Range(0, 6);
+        spriteRenderer.sprite = diceSprites[rollNumber];
+        curBounces++;
+        if (curBounces == bounces) {
+            rb2d.drag = 5;
+            rb2d.angularDrag = 5;
+            rolling = false;
+        }
+        Debug.Log("wall!");
+    }
+
+    void OnCollisionExit2D()
+    {
+        Debug.Log("exit!");
     }
 }
